e731074d38a22cfd0006b4a3a877633756f48b8b
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-10 19:45:36 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+input.onButtonEvent(Button.A, input.buttonEventValue(ButtonEvent.LongClick), function () {
+    radio.setGroup(223)
+    basic.showString("A")
+})
+input.onButtonEvent(Button.AB, input.buttonEventValue(ButtonEvent.LongClick), function () {
+    radio.setGroup(222)
+    basic.showString("C")
+})
 radio.onReceivedNumber(function (receivedNumber) {
     Calli2bot.fahreJoystick(receivedNumber, true, false, 20)
+})
+input.onButtonEvent(Button.B, input.buttonEventValue(ButtonEvent.LongClick), function () {
+    radio.setGroup(224)
+    basic.showString("B")
 })
 let Calli2bot: calli2bot.Calli2bot = null
 Calli2bot = calli2bot.beimStart(calli2bot.calli2bot_eADDR(calli2bot.eADDR.CB2_x22))
 radio.setFrequencyBand(1)
 radio.setGroup(222)
-basic.showNumber(222)
+basic.showString("C")
